66d7d9c688ed5c86df8ad4c6350fde0deadbdbc6
Bread <77288737+lucasammer@users.noreply.github.com>
2023-12-18 19:50:31 +0100

    REFCATOR: added my own math functions; changed way we initialize rect

--- src/renderer.cpp
+++ src/renderer.cpp
@@ -2,7 +2,40 @@
 #include "pos.h"
 #include "eadkpp.h"
 #include "palette.h"
-#include <cmath>
+
+namespace renderingMaths{
+    float factorial(int n) {
+        if(n == 0 || n == 1){
+            return 1.0f;
+        }else{
+            return n * factorial(n - 1);
+        }
+    }
+
+    float pow(int base, int exp){
+        float result = 1.0f;
+        for (int i = 0; i < exp; i++)
+        {
+            result *= base;
+        }
+        return result;
+    }
+
+    // sin and cos are based on taylor series
+    float sin(float angle){
+        float result = 0.0f;
+        for (int i = 0; i < 10; i++)
+        {
+            result += (pow(-1, i) / factorial(2*i+1)) * pow(angle, 2 * i + 1);
+        }
+        return result;
+    }
+
+    float cos(float angle){
+        return sin(90 - angle);
+    }
+
+} // renderingMaths
 
 namespace renderer
 {
@@ -15,16 +48,17 @@ namespace renderer
             float dy = points[i].y - target.pos.y;
 
             // Rotate the point around the camera position
-            transformedPoints[i].x = target.pos.x + dx * cos(target.rotation.yaw) - dy * sin(target.rotation.yaw);
-            transformedPoints[i].y = target.pos.y + dx * sin(target.rotation.yaw) + dy * cos(target.rotation.yaw);
+            transformedPoints[i].x = target.pos.x + dx * renderingMaths::cos(target.rotation.yaw) - dy * renderingMaths::sin(target.rotation.yaw);
+            transformedPoints[i].y = target.pos.y + dx * renderingMaths::sin(target.rotation.yaw) + dy * renderingMaths::cos(target.rotation.yaw);
             transformedPoints[i].z = points[i].z;
         }
 
-        EADK::Rect rect = {
+        EADK::Rect rect(
             static_cast<uint16_t>(transformedPoints[0].x),
             static_cast<uint16_t>(transformedPoints[0].y),
             static_cast<uint16_t>(transformedPoints[2].x - transformedPoints[0].x),
-            static_cast<uint16_t>(transformedPoints[2].y - transformedPoints[0].y)};
+            static_cast<uint16_t>(transformedPoints[2].y - transformedPoints[0].y)
+        );
 
         EADK::Display::pushRect(rect, &Purple);
     }
