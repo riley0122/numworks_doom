337b6d5bebcad6b917e5d13ca3046c288671c735
Bread <77288737+lucasammer@users.noreply.github.com>
2023-12-19 12:42:21 +0100

    FEAT: added second square

--- src/main.cpp
+++ src/main.cpp
@@ -26,6 +26,13 @@ int main(int argc, char *argv[])
         {50.0f, 80.0f, 0.0f}    // Bottom left
     };
 
+    position secondSquarePoints[4] = {
+        {50.0f, 50.0f, -1.1f},  // Top left
+        {80.0f, 50.0f, -1.1f},   // Top right
+        {80.0f, 80.0f, -1.1f},    // Bottom right
+        {50.0f, 80.0f, -1.1f}    // Bottom left
+    };
+
     while (running)
     {
         // exit condition
@@ -51,10 +58,11 @@ int main(int argc, char *argv[])
         if(kbd.keyDown(EADK::Keyboard::Key::Plus)){
             camera.pos.z += 0.05;
         }
-        
+
         EADK::Display::pushRectUniform(EADK::Screen::Full, Black);
 
         renderer::render_quad(squarePoints, camera);
+        renderer::render_quad(secondSquarePoints, camera);
 
         EADK::Timing::msleep(16);
     }
