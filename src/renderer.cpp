d2d9db211f89b85b20cfb14e886439d79bdf1654
Bread <77288737+lucasammer@users.noreply.github.com>
2023-12-19 12:01:18 +0100

    chore: time to rewrite the projection from scratch, yay

--- src/renderer.cpp
+++ src/renderer.cpp
@@ -40,26 +40,6 @@ namespace renderingMaths{
 namespace renderer
 {
     void render_quad(position points[4], Camera target){
-        position transformedPoints[4];
-        for (int i = 0; i < 4; ++i)
-        {
-            // Rotate the points based on the camera's yaw
-            float dx = points[i].x - target.pos.x;
-            float dy = points[i].y - target.pos.y;
-
-            // Rotate the point around the camera position
-            transformedPoints[i].x = target.pos.x + dx * renderingMaths::cos(target.rotation.yaw) - dy * renderingMaths::sin(target.rotation.yaw);
-            transformedPoints[i].y = target.pos.y + dx * renderingMaths::sin(target.rotation.yaw) + dy * renderingMaths::cos(target.rotation.yaw);
-            transformedPoints[i].z = points[i].z;
-        }
-
-        EADK::Rect rect(
-            static_cast<uint16_t>(transformedPoints[0].x),
-            static_cast<uint16_t>(transformedPoints[0].y),
-            static_cast<uint16_t>(transformedPoints[2].x - transformedPoints[0].x),
-            static_cast<uint16_t>(transformedPoints[2].y - transformedPoints[0].y)
-        );
-
-        EADK::Display::pushRectUniform(rect, Purple);
+        
     }
 } // namespace renderer
