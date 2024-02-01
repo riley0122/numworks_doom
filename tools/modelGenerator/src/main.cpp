f6f78f1b480f5bc9e008eb3dff487fa756e76cca
Bread <77288737+lucasammer@users.noreply.github.com>
2024-02-01 12:09:57 +0100

    FEAT: added opening file

--- tools/modelGenerator/src/main.cpp
+++ tools/modelGenerator/src/main.cpp
@@ -1,9 +1,19 @@
 #include <iostream>
+#include <fstream>
 
 int main(int argc, char *argv[]){
     if(argc < 3){
         std::cout << "usage: " << argv[0] << " [input file (.obj)] [output file (.h/.hpp)]\n";
         return -1;
+    }
+
+    std::fstream inFile(argv[1]);
+    if(inFile.is_open()){
+        std::cout << "Succesfully opened file " << argv[1] << "\n";
+
+        inFile.close();
+    }else{
+        std::cout << "Failed to open " << argv[1] << ". Are you sure it exists?\n";
     }
 
     return 0;
