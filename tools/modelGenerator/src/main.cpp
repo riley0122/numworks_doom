12504c5298f23f756db0c2b747cd9937f745579b
Bread <77288737+lucasammer@users.noreply.github.com>
2024-02-01 21:29:54 +0100

    CHORE: cleaned up some code

--- tools/modelGenerator/src/main.cpp
+++ tools/modelGenerator/src/main.cpp
@@ -5,14 +5,17 @@
 
 #include "helper.h"
 
-int main(int argc, char *argv[]){
-    if(argc < 3){
+int main(int argc, char *argv[])
+{
+    if(argc < 3)
+    {
         std::cout << "usage: " << argv[0] << " [input file (.obj)] [output file (.h/.hpp)]\n";
         return -1;
     }
 
     std::fstream inFile(argv[1]);
-    if(inFile.is_open()){
+    if(inFile.is_open())
+    {
         std::cout << "Succesfully opened file " << argv[1] << "\n";
 
         std::string line;
@@ -20,15 +23,18 @@ int main(int argc, char *argv[]){
         std::vector<std::string> pointStrings;
         std::vector<std::string> edgeStrings;
 
-        while(std::getline(inFile, line)){
-            if(line[0] == 'v' && line[1] == ' '){
+        while(std::getline(inFile, line))
+        {
+            if(line[0] == 'v' && line[1] == ' ')
+            {
                 pointStrings.push_back(line);
                 std::cout << "Found point\n";
             }
         }
 
         inFile.close();
-    }else{
+    } else
+    {
         std::cout << "Failed to open " << argv[1] << ". Are you sure it exists?\n";
     }
 
