95179d1acd6b57e88e0b10bcffab902fd12462d8
Bread <77288737+lucasammer@users.noreply.github.com>
2024-02-01 12:18:53 +0100

    FEAT: adding points to an array

--- tools/modelGenerator/src/main.cpp
+++ tools/modelGenerator/src/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <fstream>
+#include <string>
+#include <vector>
 
 int main(int argc, char *argv[]){
     if(argc < 3){
@@ -10,6 +12,18 @@ int main(int argc, char *argv[]){
     std::fstream inFile(argv[1]);
     if(inFile.is_open()){
         std::cout << "Succesfully opened file " << argv[1] << "\n";
+
+        std::string line;
+
+        std::vector<std::string> pointStrings;
+        std::vector<std::string> edgeStrings;
+
+        while(std::getline(inFile, line)){
+            if(line[0] == 'v' && line[1] == ' '){
+                pointStrings.push_back(line);
+                std::cout << "Found point\n";
+            }
+        }
 
         inFile.close();
     }else{
