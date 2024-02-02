1b60b38fe90659451ed789ad129f0ed0e03f0cfb
Bread <77288737+lucasammer@users.noreply.github.com>
2024-02-02 16:25:38 +0100

    FEAT: added writing to file; generated a cube lol

---
+++ tools/modelGenerator/cube.h
@@ -0,0 +1,42 @@
+#pragma once
+#include "pos.h"
+
+namespace Cube
+{
+	position points[8] = {
+		position{1.000,1.000,-1.000},
+		position{1.000,-1.000,-1.000},
+		position{1.000,1.000,1.000},
+		position{1.000,-1.000,1.000},
+		position{-1.000,1.000,-1.000},
+		position{-1.000,-1.000,-1.000},
+		position{-1.000,1.000,1.000},
+		position{-1.000,-1.000,1.000},
+	};
+	vertex edges[24] = {
+		vertex{0,1},
+		vertex{1,2},
+		vertex{2,3},
+		vertex{4,0},
+		vertex{0,1},
+		vertex{1,2},
+		vertex{2,3},
+		vertex{4,0},
+		vertex{0,1},
+		vertex{1,2},
+		vertex{2,3},
+		vertex{4,0},
+		vertex{0,1},
+		vertex{1,2},
+		vertex{2,3},
+		vertex{4,0},
+		vertex{0,1},
+		vertex{1,2},
+		vertex{2,3},
+		vertex{4,0},
+		vertex{0,1},
+		vertex{1,2},
+		vertex{2,3},
+		vertex{4,0},
+	};
+}
--- tools/modelGenerator/src/main.cpp
+++ tools/modelGenerator/src/main.cpp
@@ -16,6 +16,8 @@ int main(int argc, char *argv[])
     std::vector<point> points;
     std::vector<vertex> edges;
 
+    std::string name = "defualtName";
+
     std::fstream inFile(argv[1]);
     if(inFile.is_open())
     {
@@ -34,6 +36,9 @@ int main(int argc, char *argv[])
                 std::vector<vertex> found = generateEdgesFromFace(line);
                 edges.insert(edges.end(), found.begin(), found.end());
                 std::cout << "Found face with " << found.size() << " edges\n";
+            } else if (line[0] == 'o' && line[1] == ' ' && name == "defualtName")
+            {
+                name = split(line, ' ')[1];
             }
         }
 
@@ -49,7 +54,43 @@ int main(int argc, char *argv[])
     std::ofstream outFile(argv[2]);
     if(outFile.is_open())
     {
+        outFile << "#pragma once\n" << "#include \"pos.h\"\n\n" << "namespace " << name << "\n{\n";
+
+        outFile << "\t" << "position points[" << points.size() << "] = {\n";
+
+        for (int i = 0; i < points.size(); i++)
+        {
+            // 3 values each with max 3 decimals one decimal point 3 characters as base and a negative sign so max 8 per number
+            // 10 for position and brackets
+            // 9 for newlines tabs and commas
+            int lineAlloc = (8 * 3 + 10 + 9);
+            char *string = (char *) malloc(lineAlloc * sizeof(char));
+            snprintf(string, lineAlloc, "\t\tposition{%.3f,%.3f,%.3f},\n", points[i].x, points[i].y, points[i].z);
+            outFile << string;
+        }
+
+        outFile << "\t" << "};\n";
+
+        outFile << "\t" << "vertex edges[" << edges.size() << "] = {\n";
+
+        for (int i = 0; i < edges.size(); i++)
+        {
+            // 3 characters reserved for each number, dont expect more than 999 edges
+            // 8 for vertex and brackets
+            // 9 for newlines tabs and commas
+            int lineAlloc = (2 * 3 + 8 + 9);
+            char *string = (char *) malloc(lineAlloc * sizeof(char));
+            snprintf(string, lineAlloc, "\t\tvertex{%d,%d},\n", edges[i].from, edges[i].to);
+            outFile << string;
+        }
+
+        outFile << "\t" << "};\n";
+
+        outFile << "}\n";
+
         outFile.close();
+
+        std::cout << "Wrote " << argv[2] << " succesfully!\n";
     } else
     {
         std::cout << "Failed to create or open " << argv[2] << "!\n";
