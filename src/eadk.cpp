07d153b3a79a72e56bb81cad0aa826ac577fde90
Bread <77288737+riley0122@users.noreply.github.com>
2024-02-13 12:05:51 +0100

    FEAT: added license notice

--- src/eadk.cpp
+++ src/eadk.cpp
@@ -3,6 +3,10 @@
     IT IS NOT INCLUDED IN THE ACUAL BUILD.
 
     these functions will say when they are called, and return a dummy value.
+
+    This Source Code Form is subject to the terms of the Mozilla Public
+License, v. 2.0. If a copy of the MPL was not distributed with this
+file, You can obtain one at https://mozilla.org/MPL/2.0/.
 */
 #include <stdbool.h>
 #include <stddef.h>
