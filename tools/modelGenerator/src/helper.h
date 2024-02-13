538e8db40f46fe7edb152b1af01da0ba7fe1b3ac
Bread <77288737+riley0122@users.noreply.github.com>
2024-02-13 11:59:36 +0100

    CHORE: added compact license notice

--- tools/modelGenerator/src/helper.h
+++ tools/modelGenerator/src/helper.h
@@ -1,3 +1,16 @@
+/*
+  This Source Code Form is "Incompatible With Secondary Licenses," as
+  defined by the Mozilla Public License, v. 2.0.
+
+  This specific component is licensed under the GNU General Public License
+  (GPL). Please refer to the 'LICENSE' file in this directory for the
+  applicable terms of the GPL.
+
+    Copyright (C) 2024 contributers
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
 #pragma once
 
 #include <string>
